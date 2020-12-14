9743533199783438e501460518bcd640bc153a29
Cheng-Hung Tsai <ctsai085@ucr.edu>
2020-12-14 10:51:43 -0800

    Add sbatch job-array support

--- bin/6_HMMRATAC.bash
+++ bin/6_HMMRATAC.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH --job-name=6_HMMRATAC
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --mem=64gb
+#SBATCH --array=1-4
+#SBATCH --output=sbatch_log/%x_%A_%a.out
 
 CPU=4
 INPUT_DIR=$DIR/alignment_output
@@ -7,20 +13,30 @@ mkdir -p $OUTPUT_DIR
 
 GENOMEINFO=$GENOME.hmmratac.info
 
+# Generate GENOMEINFO, a two-column, tab delimited file with chromosome ID and length
 if [ ! -f $GENOMEINFO ]
 then
 	samtools faidx $GENOME -o $GENOME.fai
 	cut -f1,2 $GENOME.fai > $GENOMEINFO
 fi
 
+# [For sbatch job-array] line counter of metadata.csv
+line=1
 tail -n +2 $DIR/metadata.csv | while IFS="," read Accession Type Layout R1 R2 Strain Condition Replicate
 do
-	sample=`echo $Strain $Condition $Replicate | tr ' ' '_'`
-	final_bam="$sample.final.bam"
-    echo "Processing $sample ..."
-	HMMRATAC	-b $INPUT_DIR/$final_bam \
-				-i $INPUT_DIR/$final_bam.bai \
-				-g $GENOMEINFO \
-				-o $OUTPUT_DIR/$sample \
-				--bedgraph true --bgscore true --trim 1 --window 1000000
+	# if not job-array scheduled then do while loop normally
+	# if job-array scheduled then only execute the loop that line and SLURM_ARRAY_TASK_ID are matched
+	if [ -z $SLURM_ARRAY_TASK_ID ] || [ $line -eq $SLURM_ARRAY_TASK_ID ]
+	then
+		sample=`echo $Strain $Condition $Replicate | tr ' ' '_'`
+		final_bam="$sample.final.bam"
+		echo $line
+		echo "Processing $sample ..."
+		HMMRATAC	-b $INPUT_DIR/$final_bam \
+					-i $INPUT_DIR/$final_bam.bai \
+					-g $GENOMEINFO \
+					-o $OUTPUT_DIR/$sample \
+					--bedgraph true --bgscore true --trim 1 --window 1000000
+	fi
+	let line+=1
 done
