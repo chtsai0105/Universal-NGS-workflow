3fae59a327875ea17984e9682e121e3ed878b66e
Cheng-Hung Tsai <ctsai085@ucr.edu>
2020-12-14 21:25:30 -0800

    Separate env and reference downloading script

--- bin/0_environment.bash
+++ bin/0_environment.bash
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-DIR=/home/chtsai/2020_project-chtsai0105
+DIR=/rhome/ctsai085/2020_project-chtsai0105
 BIN=$DIR/bin
 REF=$DIR/ref
 RAW=$DIR/raw
+# Genome reference (For ATAC-seq, ChIP-seq and DNase-seq pipeline)
 GENOME=$REF/S288C_reference_sequence_R64-2-1_20150113.fsa
-# TXTOME=$REF/AaegL5.0_CDS.fa.gz
-
-# Download genome assembly
-# if [ ! -f $GENOME ]
-# then
-    # curl -o $GENOME https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/204/515/GCF_002204515.2_AaegL5.0/GCF_002204515.2_AaegL5.0_genomic.fna.gz
-    # curl -o $GENOME http://sgd-archive.yeastgenome.org/sequence/S288C_reference/genome_releases/S288C_reference_genome_Current_Release.tgz
-# fi
-
-# Download transcriptome
-# if [ ! -f $TXTOME ]
-# then
-#     curl -o $TXTOME https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/204/515/GCF_002204515.2_AaegL5.0/GCF_002204515.2_AaegL5.0_cds_from_genomic.fna.gz
-# fi
+# Transcriptome reference (For RNA-seq pipeline)
+# TXTOME=
 
 export DIR=$DIR REF=$REF RAW=$RAW BIN=$BIN GENOME=$GENOME
