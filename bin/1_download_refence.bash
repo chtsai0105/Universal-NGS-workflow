d472ee221b258435c1da3b1cf157f139b47bc96d
Cheng-Hung Tsai <ctsai085@ucr.edu>
2020-12-17 01:10:49 -0800

    Remove unused url

--- bin/1_download_refence.bash
+++ bin/1_download_refence.bash
@@ -3,12 +3,11 @@
 # Download genome assembly
 if [ -n $GENOME ] && [ ! -f $GENOME ]
 then
-    curl -o $GENOME https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/204/515/GCF_002204515.2_AaegL5.0/GCF_002204515.2_AaegL5.0_genomic.fna.gz
     curl -o $GENOME http://sgd-archive.yeastgenome.org/sequence/S288C_reference/genome_releases/S288C_reference_genome_Current_Release.tgz
 fi
 
 # Download transcriptome
 if [ -n TXTOME ] && [ ! -f $TXTOME ]
 then
-    curl -o $TXTOME https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/204/515/GCF_002204515.2_AaegL5.0/GCF_002204515.2_AaegL5.0_cds_from_genomic.fna.gz
+    curl -o $TXTOME
 fi
